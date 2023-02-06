595a256e695648f1cdc76bed3beb1c2fff40e5b1
dahezhiquan <3390205563@qq.com>
2023-02-06 16:34:11 +0800

    新增项目入口文件

---
+++ HouseRentApp.java
@@ -0,0 +1,13 @@
+import view.HouseView;
+
+/**
+ * 项目入口
+ *
+ * @author dahezhiquan
+ */
+public class HouseRentApp {
+    public static void main(String[] args) {
+        new HouseView().mainMenu(); // 显示菜单
+        System.out.println("===============已退出房屋出租系统===============");
+    }
+}
